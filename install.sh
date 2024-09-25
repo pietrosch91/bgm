eb949665da1c5f9752cf39d8981ff2a454e34427
Pietro Ottanelli <pietro.ottanelli@pi.infn.it>
2024-09-25 10:11:35 +0200

    Minor changes in the visualization of the user collection

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo mysql -e "CREATE USER IF NOT EXISTS 'fi-gioca-admin'@'localhost' IDENTIFIED BY 'fi-gioca-admin'"
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS bgm2"
-sudo mysql -e "GRANT ALL ON bgm2.* TO 'fi-gioca-admin'@'localhost';"
+sudo mysql -e "GRANT ALL ON bgm2.* TO 'fi-gioca-admin'@'localhost'"
+sudo mysql -e "DELETE FROM mysql.procs WHERE db = 'bgm2'"
---
+++ sql_procedures/collection/functions/collection_available_to_f.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION `collection_available_to_f`(gid INT) RETURNS INT
+READS SQL DATA
+BEGIN
+    DECLARE usr INT DEFAULT NULL;
+    DECLARE foster INT DEFAULT NULL;
+    IF(collection_test_f(gid,FALSE)!= 1) THEN
+        RETURN NULL;
+    END IF;
+    SELECT col_Owner_ID into usr, col_Foster_ID into foster FROM BGM_Collection WHERE col_ID=gid;
+    IF(foster IS NOT NULL) THEN
+        RETURN foster;
+    END IF;
+    RETURN usr;
+END
